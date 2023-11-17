275bd8cfd414bc760e3fcb170b66cb05c9a326e1
Caliche2399 <carlosaguilarpaniagua@gmail.com>
2023-11-17 08:47:57 -0600

    Se agregan las rutas para manejar los heroes

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,11 +1,10 @@
+import { AppRouter } from "./router/AppRouter"
 
 
 export const HeroesApp = () => {
   return (
     <>
-        <h1>
-            Heroes APP
-        </h1>
+        <AppRouter></AppRouter>
     </>
   )
 }
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Routes, Route, Navigate} from 'react-router-dom'
+import { MarvelPage } from '../heroes/pages/MarvelPage'
+import { DcPage } from '../heroes/pages/DcPage'
+import { LoginPage } from '../auth/pages/LoginPage'
+
+export const AppRouter = () => {
+  return (
+    <>
+        <Routes>
+            <Route path='/marvel' element={<MarvelPage/>}></Route>
+            <Route path='/dc' element={<DcPage/>}></Route>
+            
+            <Route path='/login' element={<LoginPage/>}></Route>
+            
+            <Route path='/' element={<Navigate to="/marvel"/>}></Route>
+        </Routes>
+    </>
+  )
+}
